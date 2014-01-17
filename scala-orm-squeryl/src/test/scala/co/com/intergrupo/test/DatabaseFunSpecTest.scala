f5042d7739f827e6082e44f871d6cf32aeaca7a0
nefeper <nefeper@hotmail.com>
2014-01-17 17:22:44 -0500

    Agregando DatabaseFunsuiteTest y DatabaseFunSpecTest para realizar pruebas unitarias, se modifo parte del modelo para estabilizar version

---
+++ scala-orm-squeryl/src/test/scala/co/com/intergrupo/test/DatabaseFunSpecTest.scala
@@ -0,0 +1,41 @@
+package co.com.intergrupo.test
+
+import org.scalatest.{ FunSpec, BeforeAndAfter }
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import co.com.intergrupo.database._
+import co.com.intergrupo.entities._
+import org.squeryl.PrimitiveTypeMode.transaction
+
+@RunWith(classOf[JUnitRunner])
+class MyFunSpecTest extends FunSpec with BeforeAndAfter with TraitConnection {
+
+  before {
+    connect
+    transaction {
+      SchemaSubasta.drop
+      SchemaSubasta.create
+    }
+  }
+
+  describe("insertando configuracion inicial ") {
+    it("configurando el mail.smtp.host") {
+      transaction {
+        val param1: Config = new Config("mail.smtp.host", "smtp.gmail.com")
+        assert(param1.id == 0)
+        SchemaSubasta.config.insert(param1)
+        assert(param1.id > 0)
+      }
+    }
+  }
+
+  //describe("Falta implementar") {
+   // it("prueba pendiente")(pending)
+  // }
+
+  after {
+    close
+  }
+
+}
---
+++ scala-orm-squeryl/src/test/scala/co/com/intergrupo/test/DatabaseFunSuiteTest.scala
@@ -0,0 +1,36 @@
+package co.com.intergrupo.test
+
+import org.scalatest.{ FunSuite, BeforeAndAfter }
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSpec
+
+import co.com.intergrupo.database._
+import co.com.intergrupo.entities._
+import org.squeryl.PrimitiveTypeMode.transaction
+
+@RunWith(classOf[JUnitRunner])
+class DatabaseFunSuiteTest extends FunSuite with BeforeAndAfter with TraitConnection {
+
+  before {
+    connect
+    transaction {
+      SchemaSubasta.drop
+      SchemaSubasta.create
+    }
+  }
+
+  test("insertando configuracion inicial configurando el mail.smtp.host ") {
+    transaction {
+      val param1: Config = new Config("mail.smtp.host", "smtp.gmail.com")
+      assert(param1.id == 0)
+      SchemaSubasta.config.insert(param1)
+      assert(param1.id > 0)
+    }
+  }
+
+  after {
+    close
+  }
+
+}
